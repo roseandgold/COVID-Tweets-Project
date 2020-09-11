275cc3692a0acc3dbf845886747f117f9aa9d154
roseandgold <39243828+roseandgold@users.noreply.github.com>
2020-09-10 21:39:30 -0700

    Update twitterAPIScript.py

--- Data-Gathering-Scripts/twitterAPIScript.py
+++ Data-Gathering-Scripts/twitterAPIScript.py
@@ -1,6 +1,23 @@
-# Twitter API script
-# Takes a CSV of Tweet ids, accesses the Twitter API to get the Tweet text, and returns a CSV with the date, text, location, language, hashtags and sentiment score.
-# The main functions that need to be run to use the script are the getTwitterDf function and the concatDfs function. The rest of the functions are used within the getTwitterDf function
+'''
+Takes a CSV of Tweet ids, accesses the Twitter API to get the Tweet text,
+and returns a CSV with the date, text, location, language, hashtags, sentiment score and if it 
+an original tweet or a re-tweet. The API call gathers up to 100 Tweets at a time.
+
+Need to create a "tweet_data" folder so the script can output the CSVs to this folder.
+Need to create a text file named "twitterApiSecrets.txt" which includes:
+    {"token": <token>, "tokenSecret": <token Secret>, "consumerKey": <consumer key>, 
+    "consumerSecret": <consumer secret}
+
+Keyword arguments:
+tweetIdFile -- filepath to a csv which contains the Tweet Ids and sentiment scores
+nextTweet -- the index number of the Tweet in the file you want to start processing
+             needs to be a multiple of 100
+numTweet -- the number of Tweets you want to process, needs to be a multiple of 100
+
+Laura Stagnaro, Ian Byrne
+SIADS 591 & 592 Milestone I
+Coronavirus Tweet Analysis Project
+'''
 
 # Import the necessary libraries
 import tweepy
@@ -8,12 +25,8 @@ import json
 import pandas as pd
 import time
 from collections import defaultdict
-
-# Create the necessary variables
-tweetIdFile = 'corona_tweets_01.csv' # This is the CSV file which contains all the Tweets IDs that the Twitter API needs to return
-twitterDf = None
-tokenSecretFile = 'twitterApiSecrets.txt' # This needs to be a text file with a dictionary containing the keys: consumerKey, 
-                                          # consumerSecret, token, and tokenSecret.
+import sys
+import os
     
 def setupApi(tokenSecretFile):
     '''Sets up the Twitter API.
@@ -60,10 +73,10 @@ def getTweetIds(tweetIdFile):
     
     '''
     # Create a dataframe with the tweet ids and sentiment scores
-    tweetIdDf = pd.read_csv(tweetIdFile, header=None, names=['tweet_ids', 'sentiment_scores'])
+    tweetIdDf = pd.read_csv(tweetIdFile, header=None, names=['tweet_id', 'sentiment_score'])
     
     # Get a list of lists for the tweet ids. Each list contains 100 tweet ids
-    tweetIds = tweetIdDf['tweet_ids'].to_list()
+    tweetIds = tweetIdDf['tweet_id'].to_list()
     total100s = len(tweetIds)//100 # Get the number of lists of 100 tweet ids
     remainder = len(tweetIds) % 100 # This is the number of tweet ids which will be in the last list
     startEnd = zip(range(0, len(tweetIds)-remainder, 100),range(100, len(tweetIds), 100)) # Create a zip of tuples with what index to start and stop at to create the list of 100 tweet ids 
@@ -72,7 +85,7 @@ def getTweetIds(tweetIdFile):
         tweetIdsList.append(list(tweetIds[len(tweetIds)-remainder : len(tweetIds)])) # Append the last list
     
     # Create a dictionary which includes the tweet ids as the key and sentiment score as the value
-    sentimentDict = dict(zip(tweetIdDf['tweet_ids'].to_list(), tweetIdDf['sentiment_scores'].to_list()))
+    sentimentDict = dict(zip(tweetIdDf['tweet_id'].to_list(), tweetIdDf['sentiment_score'].to_list()))
     
     # Print out the number of Tweet ids in the list
     print('There are {} Tweet Ids'.format(len(tweetIdDf)))
@@ -96,15 +109,27 @@ def parseTweet(tweetContent):
     # Get the necessary information from the tweet content
     idVal = tweetContent.id # Get the id of the tweet
     timestamp = tweetContent.created_at # Returns a datetime timestamp
-    text = tweetContent.text # Returns a string
-    hashtag = tweetContent.entities['hashtags'] # Returns a list with the hashtags
     location = tweetContent.user.location # Returns a string
     lang = tweetContent.lang # Get the language of the Tweet
+
+    # Create a list with the hashtags
+    hashtags = ''
     
-    return idVal, timestamp, text, hashtag, location, lang
+    for tag in tweetContent.entities['hashtags']:
+        hashtags = hashtags + tag['text'] + ", "
+
+    # Get the tweet text
+    if "retweeted_status" in tweetContent._json:
+        text = tweetContent.retweeted_status.full_text
+        status = 'Retweet'
+    else:
+            text = tweetContent.full_text # Returns a string
+            status = 'Original'
+    
+    return idVal, timestamp, text, hashtags, location, lang, status
 
 
-def main(tweetIdFile, tokenSecretFile, nextTweet = 0, numTweets = 200):
+def main(tweetIdFile, nextTweet, numTweets):
     '''Create a dataframe which includes all the necessary Tweet information. The API searches for 100 tweet ids at
     one time.
     
@@ -119,6 +144,12 @@ def main(tweetIdFile, tokenSecretFile, nextTweet = 0, numTweets = 200):
     twitterDf -- a dataframe with all necessary Twitter information
     
     '''
+    # Create the variables
+    nextTweet = int(nextTweet)
+    numTweets = int(numTweets)
+    tokenSecretFile = 'twitterApiSecrets.txt' # This needs to be a text file with a dictionary containing the keys: consumerKey, 
+                                          # consumerSecret, token, and tokenSecret.
+    
     # Get the Tweet Ids and sentiment scores
     tweetIds, sentimentDict = getTweetIds(tweetIdFile)
     
@@ -142,17 +173,18 @@ def main(tweetIdFile, tokenSecretFile, nextTweet = 0, numTweets = 200):
                 print('Rate Limit Remaining: {}'.format(rateLimitRemaining)) # Print the rate limit remaining every 100 searches
             
             # Access the API and get the Tweet information
-            tweetJson = api.statuses_lookup(tweetGroup) # Gets the json for all 100 tweets in the tweetGroup
+            tweetJson = api.statuses_lookup(tweetGroup, tweet_mode='extended') # Gets the json for all 100 tweets in the tweetGroup
             try: 
                 # Iterate through each tweet json to get the information
                 for tweet in tweetJson:
-                    idVal, timestamp, text, hashtag, location, lang = parseTweet(tweet)
-                    tweetDict['id'].append(idVal)
+                    idVal, timestamp, text, hashtags, location, lang, status = parseTweet(tweet)
+                    tweetDict['id'].append(str(idVal))
                     tweetDict['timestamp'].append(timestamp)
                     tweetDict['text'].append(text)
-                    tweetDict['hashtag'].append(hashtag)
+                    tweetDict['hashtag'].append(hashtags)
                     tweetDict['location'].append(location)
                     tweetDict['lang'].append(lang)
+                    tweetDict['status'].append(status)
                     tweetDict['sentimentScore'].append(sentimentDict[idVal])
 
             except tweepy.TweepError as e:
@@ -171,18 +203,20 @@ def main(tweetIdFile, tokenSecretFile, nextTweet = 0, numTweets = 200):
     # Write the dataframe to a csv
     directory = 'tweet_data/'
     files = os.listdir(directory)
+
     if len(files) == 0:
-           filename = directory + 'covidTweets_1_.csv'
+        fileName = directory + 'covidTweets_1_.csv'
     else:
       fileNum = str(int(files[-1].split('_')[1]) + 1)
-      fileName = dicrectory + 'covidTweets_' + fileNum + '_.csv'
-    twitterDf.to_csv(fileName)
+      fileName = directory + 'covidTweets_' + fileNum + '_.csv'
+    twitterDf.to_csv(fileName, index=False)
       
     
     # Print out the next Tweet id need to start at and the number that are left
     rateLimitRemaining = api.rate_limit_status()['resources']['statuses']['/statuses/lookup']['remaining']
     print('Rate Limit Remaining: {}'.format(rateLimitRemaining)) # Print the number of searches left
     print('Next start: {}'.format((start + numIterations) * 100))
+    print('File {} created'.format(fileName))
 
-if__name__ == "__main__"
-  main()
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
