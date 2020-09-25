c7551b1008ace9675450b69aad4321858ca27629
Laura Stagnaro <rose.and.gold07@gmail.com>
2020-09-24 18:12:09 -0700

    Added average columns, phase column, and merge with sentiment score data

--- Data-Gathering-Scripts/covidTimeSeries.py
+++ Data-Gathering-Scripts/covidTimeSeries.py
@@ -51,6 +51,10 @@ def main(covidFile, allTweetsFile):
     # Change the date to datetime object to change date format in csv
     covid['Date'] = pd.to_datetime(covid['Date'])
 
+    # Filter down to the necessary dates
+    covid = covid[(covid['Date'] >= '2020-03-01') & (covid['Date'] <= '2020-09-01')]
+    covid['Date'] = covid['Date'].dt.date
+
     # Groupby date to get the total number of covid cases per day
     covid = covid.groupby('Date').agg({'Confirmed':sum}).reset_index()
 
@@ -63,14 +67,13 @@ def main(covidFile, allTweetsFile):
     # Get the min max scale for rolling average
     avgConfirmedCases = covid['New Cases 7 Day Rolling Average'].values.reshape(-1,1)
     avgConfirmedCasesScaled = MinMaxScaler().fit_transform(avgConfirmedCases)
-    covid['New Cases 7 Day Rolling Average (min-max scaled'] = avgConfirmedCasesScaled
+    covid['New Cases 7 Day Rolling Average (min-max scaled)'] = avgConfirmedCasesScaled
 
     # Get the sentiment score
     sentiment = pd.read_csv(allTweetsFile, usecols = ['timestamp', 'sentimentScore'])
     
     # Create a date column and filter down to date and sentiment score columns
     sentiment['Date'] = pd.to_datetime(sentiment['timestamp']).dt.date
-    sentiment['Date'] = pd.to_datetime(sentiment['Date'])
     sentiment = sentiment[['Date', 'sentimentScore']]
 
     # Calculate the average sentiment score per day
@@ -82,13 +85,38 @@ def main(covidFile, allTweetsFile):
     # Calculate 7 day rolling average sentiment min max scaled
     avgSentiment = sentiment['Sentiment 7 Day Rolling Average'].values.reshape(-1,1)
     avgSentimentScaled = MinMaxScaler().fit_transform(avgSentiment)
-    sentiment['Sentiment 7 Day Rolling Average (min-max scaled'] = avgSentimentScaled
+    sentiment['Sentiment 7 Day Rolling Average (min-max scaled)'] = avgSentimentScaled
 
-    # Merge the dataframes on date to add the sentiment score
+    # Merge the dataframes on date to add the sentiment score data
     covid = covid.merge(sentiment, how = 'outer', on='Date')
 
     # Rename columns
     covid.rename(columns = {'sentimentScore': 'Average Sentiment Score'}, inplace=True)
+
+    # Add what covid phase the count fell into
+    # Create a list of dates for each phase
+    phase1Dates = list(pd.date_range('2020-03-01', '2020-04-07', freq = 'D'))
+    phase2Dates = list(pd.date_range('2020-04-08', '2020-05-12', freq = 'D'))
+    phase3Dates = list(pd.date_range('2020-05-13', '2020-07-28', freq = 'D'))
+    phase4Dates = list(pd.date_range('2020-07-29', '2020-09-01', freq= 'D'))
+
+    # Put the list of dates into one list
+    allDates = phase1Dates + phase2Dates + phase3Dates + phase4Dates
+
+    # Create lists with the phase labels
+    phase1 = ['phase 1'] * len(phase1Dates)
+    phase2 = ['phase 2'] * len(phase2Dates)
+    phase3 = ['phase 3'] * len(phase3Dates)
+    phase4 = ['phase 4'] * len(phase4Dates)
+
+    # Put the list of labels into one list
+    allPhases = phase1 + phase2 + phase3 + phase4
+
+    # Create a dictionary with the phases and dates
+    phaseDict = dict(zip(allDates, allPhases))
+
+    # Create the phase label column
+    covid['covid phase'] = covid['Date'].map(phaseDict)
 
     # Write the dataframe to a csv
     covid.to_csv('covidTimeSeries.csv', index=False)
